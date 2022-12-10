18a643b64fd23f9af58aade698a04d2e698e440b
Sumant <sumantb22@gmail.com>
2022-12-11 00:21:38 +0530

    updated

---
+++ src/demogame/Myapp.java
@@ -0,0 +1,14 @@
+package demogame;
+import javax.swing.JFrame;
+public class Myapp {
+    public static void main(String[] arg){
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
